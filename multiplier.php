1fbb78db0acee8e743ef9b708396a26dbe2bf436
HENRY BENAYE <2106747@talnet.nl>
2020-01-14 15:03:02 +0100

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Ik ga twee getallen met elkaar vermenigvuldigen". PHP_EOL;
+echo "Eerste getal";
+$a = readline("");
+echo "Tweede getal";
+$b = readline("");
+echo ("U resultaat:") . $a * $b . PHP_EOL; 
+
+
+
+
+
+
